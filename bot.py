ac6796dc02c9ae0c20745c0a46350da4d2f8a4dd
kum4r2k <65287009+kum4r2k@users.noreply.github.com>
2020-07-18 21:58:31 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+client=commands.Bot(command_prefix='!')
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+@client.event
+async def on_member_join(member):
+    print("A wild",member,"appeared")
+
+@client.event
+async def on_member_remove(member):
+    print("The wild",member,"disappeared")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Hey! I am up and running with a ping of {round(client.latency*1000)}ms.")
+
+client.run('NzMzNzEzMzcyNjg3ODkyNDgx.XxHK7g.0Kd7aPL05GcVTrT5cGZ520Zm2wo')
